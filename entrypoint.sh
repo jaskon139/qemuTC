62b67ce16e2788bad991c62b0562d36d36cfcd9a
jaskon139 <jaskon139@gmail.com>
2018-08-17 14:08:16 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 qemu-system-x86_64 -nographic -net nic,vlan=0 -net user -m 512 -hda /app/tc.img < /dev/null &
 
 
-cd /opt/wetty && node /opt/wetty/app.js -p 3000
+cd /opt/wetty && node /opt/wetty/app.js -p $PORT
