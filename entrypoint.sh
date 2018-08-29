639e5b5bfe9c9dea780221c740bdb5d167a5e751
jaskon139 <jaskon139@gmail.com>
2018-08-29 22:16:36 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-qemu-system-x86_64 -nographic -net nic,vlan=0 -net user -m 256 -hda /app/tc.img < /dev/null &
+qemu-system-x86_64 -nographic -net nic,vlan=0 -net user,hostfwd=tcp:127.0.0.1:8888-:22 -m 256 -hda /app/tc.img < /dev/null &
 
 
 /usr/local/bin/gotty --port $PORT --permit-write --reconnect /bin/sh
